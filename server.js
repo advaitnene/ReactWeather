c8e74c27a7e0839feca9f3de81c7fd7904a4f949
advaitnene <advaitnene@gmail.com>
2020-11-29 11:43:59 +0530

    sever.js changes related to https and redirect URL

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ var express = require('express');
 
 //Create our app
 var app = express();
+
+// Use of environment variable to provide dynamic port number.
 const PORT = process.env.PORT || 3000;
 
+//openweathermap api only supports http traffic. To redirect https traffic, the following function is used.
 app.use(function(req, res, next) {
-  if(req.headers['x-forwarded-proto'] === 'http'){
-    next();
-  } else {
+  if(req.headers['x-forwarded-proto'] === 'https'){
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
