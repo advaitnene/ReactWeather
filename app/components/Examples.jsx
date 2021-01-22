e0e6dc29fb212216fc603a73e866b755b784965e
advaitnene <advaitnene@gmail.com>
2021-01-22 14:39:17 +0530

    UI changes with Foundation

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = React.createClass({
   render: function() {
     return (
       <div>
-        <h2>Examples</h2>
-        <p>Welcome to the Examples page!</p>
+        <h1 className="text-centered">Examples</h1>
+        <p>Here are a few locations to try out:</p>
+        <ol>
+          <li>
+            <Link to='/?location=Mumbai'>Mumbai</Link>
+          </li>
+          <li>
+            <Link to='/?location=Pune'>Pune</Link>
+          </li>
+        </ol>
       </div>
     );
   }
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,13 +5,39 @@ var {Link, IndexLink} = require('react-router');
 extra styling properties to the links with Reacts in-built classes */
 
 var Nav = React.createClass({
+  onSearch: function(e){
+    e.preventDefault();
+    alert('Code yet to be written!');
+  },
   render: function() {
     return (
-      <div>
-        <h2>Nav Component</h2>
-        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-        <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onSearch}>
+            <ul className="menu">
+              <li>
+                <input type="search" placeholder="Search weather"></input>
+              </li>
+              <li>
+                <input type="submit" className="button" value="Get Weather"></input>
+              </li>
+            </ul>
+          </form>
+        </div>
       </div>
     );
   }
